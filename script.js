1b095e9c17f47306d59749883c6635c80f72edff
shadow79198 <williamrodriguez2607@gmail.com>
2025-06-11 15:45:57 -0400

    ur ur ur

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 //  - Give it the value of your name. 
 //  - This is a string so the value should be in quotes ""
 
-
+let myName = "Freddy Fazbear"
+let age = 42
+let answer = document.querySelector(".answer")
 
 // 2. Declare a variable called age. 
 //  - Give it the value of your age. 
@@ -14,4 +16,4 @@
 
 // 4. When you're done, remove the comment from the code below.
 
-// answer.innerHTML = myName + " is " + age;
+answer.innerHTML = myName + " is " + age;
